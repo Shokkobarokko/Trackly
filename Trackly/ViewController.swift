5a4276c572e43a325a83c697a4b92bad4bc9db32
Shokkobarokko <avetis.01@mail.ru>
2026-08-26 13:42:16 +0300

    Initial Commit

---
+++ Trackly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trackly
+//
+//  Created by Аветис Парсаданян on 8/26/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
