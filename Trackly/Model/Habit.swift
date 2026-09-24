cc7537cd391e0cc8e3744bcab886f67f64f6ff02
Shokkobarokko <avetis.01@mail.ru>
2026-09-24 10:55:20 +0300

    Preserve habit identity across launches

--- Trackly/Model/Habit.swift
+++ Trackly/Model/Habit.swift
@@ -1,8 +1,18 @@
 import Foundation
 
 struct Habit: Codable {
-    let id = UUID()
+    let id: UUID
     var text: String
-    let createdDate = Date()
+    let createdDate: Date
+    
+    init(
+        id: UUID = UUID(),
+        text: String,
+        createdDate: Date = Date()
+    ) {
+        self.id = id
+        self.text = text
+        self.createdDate = createdDate
+    }
 }
 
--- Trackly/ViewModels/HabitsViewModel.swift
+++ Trackly/ViewModels/HabitsViewModel.swift
@@ -8,6 +8,7 @@ final class HabitsViewModel {
     
     init() {
         habits = HabitsStorage.load()
+        print(habits.map { ( $0.text, $0.id, $0.createdDate)})
     }
 }
 
@@ -24,8 +25,8 @@ extension HabitsViewModel {
     
     func deleteHabit(at index: Int) {
         habits.remove(at: index)
+        print("Before save:", habits.map { $0.text })
         HabitsStorage.save(habits)
         onHabitsChanged?()
-        
     }
 }
